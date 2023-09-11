e34c042609cc816e3d99c5acf594eae022334486
NePav <nenad.pavlovic@gmail.com>
2023-09-11 15:58:00 +0200

    table follows coumns structure

--- public/app.js
+++ public/app.js
@@ -1,46 +1,46 @@
-document.getElementById("login").addEventListener("click", function() {                                                                                                                         
-  fetch("/login", { method: "POST" })                                                                                                                                                           
-    .then(response => {                                                                                                                                                                         
-      if (response.ok) {                                                                                                                                                                        
-        this.style.backgroundColor = "green";                                                                                                                                                   
-        document.getElementById("get-positions").disabled = false;                                                                                                                              
-      } else {                                                                                                                                                                                  
-        this.style.backgroundColor = "red";                                                                                                                                                     
-      }                                                                                                                                                                                         
-    })                                                                                                                                                                                          
-    .catch(error => console.error("Error:", error));                                                                                                                                            
-});                                                                                                                                                                                             
-                                                                                                                                                                                                
-document.getElementById("get-positions").addEventListener("click", function() {                                                                                                                 
-  fetch("/positions")                                                                                                                                                                           
-    .then(response => response.json())                                                                                                                                                          
-    .then(data => {                                                                                                                                                                             
-      const table = $('#positions').DataTable();                                                                                                                                                
-      for (let instrument of data.instrumentPositions) {                                                                                                                                        
-        for (let position of instrument.positions) {                                                                                                                                            
-          table.row.add([                                                                                                                                                                       
-            position.accountName,                                                                                                                                                               
-            position.accountType,                                                                                                                                                               
-            position.depositable,                                                                                                                                                               
-            position.accountId,                                                                                                                                                                 
-            position.volume,                                                                                                                                                                    
-            position.profit,                                                                                                                                                                    
-            position.profitPercent,                                                                                                                                                             
-            position.averageAcquiredPrice,                                                                                                                                                      
-            position.acquiredValue,                                                                                                                                                             
-            position.value,                                                                                                                                                                     
-            position.currency,                                                                                                                                                                  
-            position.orderbookId,                                                                                                                                                               
-            position.lastPrice,                                                                                                                                                                 
-            position.lastPriceUpdated,                                                                                                                                                          
-            position.change,                                                                                                                                                                    
-            position.changePercent,                                                                                                                                                             
-            position.tradable,                                                                                                                                                                  
-            position.name,                                                                                                                                                                      
-            new Date().toLocaleString()                                                                                                                                                         
-          ]).draw();                                                                                                                                                                            
-        }                                                                                                                                                                                       
-      }                                                                                                                                                                                         
-    })                                                                                                                                                                                          
-    .catch(error => console.error("Error:", error));                                                                                                                                            
+document.getElementById("login").addEventListener("click", function() {
+  fetch("/login", { method: "POST" })
+      .then(response => {
+          if (response.ok) {
+              this.style.backgroundColor = "green";
+              document.getElementById("get-positions").disabled = false;
+          } else {
+              this.style.backgroundColor = "red";
+          }
+      })
+      .catch(error => console.error("Error:", error));
+});
+
+document.getElementById("get-positions").addEventListener("click", function() {
+  fetch("/positions")
+      .then(response => response.json())
+      .then(data => {
+          const table = $('#positions').DataTable();
+          for (let instrument of data.instrumentPositions) {
+              for (let position of instrument.positions) {
+                  table.row.add({
+                      accountName: position.accountName,
+                      accountType: position.accountType,
+                      depositable: position.depositable,
+                      accountId: position.accountId,
+                      volume: position.volume,
+                      profit: position.profit,
+                      profitPercent: position.profitPercent,
+                      averageAcquiredPrice: position.averageAcquiredPrice,
+                      acquiredValue: position.acquiredValue,
+                      value: position.value,
+                      currency: position.currency,
+                      orderbookId: position.orderbookId,
+                      lastPrice: position.lastPrice,
+                      lastPriceUpdated: position.lastPriceUpdated,
+                      change: position.change,
+                      changePercent: position.changePercent,
+                      tradable: position.tradable,
+                      name: position.name,
+                      timestamp: new Date().toLocaleString()
+                  }).draw();
+              }
+          }
+      })
+      .catch(error => console.error("Error:", error));
 });
